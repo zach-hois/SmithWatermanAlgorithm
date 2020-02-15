40816c64616aebbde03cbf37c3946e4fec8f04a3
zach-hois <zach.hoisington@ucsf.edu>
2020-02-14 17:07:47 -0800

    pre flight

--- smith_waterman/algs.py
+++ smith_waterman/algs.py
@@ -17,11 +17,12 @@ def sw(seq1, seq2):
 	#aligning the sequences
 	alignedSeq1, alignedSeq2 = path(scoreMatrix, startPosition)
 
-
+	# print the results 
+	length1 = len(alignedSeq1)
+ 	#saving this for last lol
     return None
 
 def newScoringMatrix(rows, cols):
-
 	"""
 	making a scoring matrix that will then be filled with the scores
 	"""
@@ -45,9 +46,31 @@ def score(matrix, x, y):
 	calculate the score for every position in the scoring matrix 
 	based on the position in the table's neighbors
 	"""
+	similarity = match if seq1[x-1] = seq2[y-1] else mismatch #this is meta to check if the two positions in a sequence match
+
+	diagonalScore = matrix[x-1][y-1] + similarity #we did something like this in class
+	upScore = matrix[x-1][y] + gap #vertical score accounting for gaps
+	leftScore = matrix[x][y-1] + gap#horizontal score accounting for gaps
+
+    return max(0, diagonalScore, upScore, leftScore) #return the best score of those calculated, this will be the way through the matrix and match up the seqs
+
+def path(): 
+	"""
+	how to decide which path to take through the scoring matrix
+	taking the best path through the matrix based on the score will 
+	align the sequences with the best match and gap sequence/ratio
+
+	diagonal move = match or mismatch
+	up move = gap in seq1
+	left move = gap in seq2
+	"""
+
+	END, DIAG, UP, LEFT = range(4)
+	alignedSeq1 = []
+	alignedSeq2 = []
+	x, y = startPosition
+	return None
 
 
-    return None
-def path(): #how to decide which path to take through the scoring matrix
 def roc():
     return None
